fe18313e5f0eb440557f65d42f7a7a11043e26e6
GabrieleLombard01 <gabriellombardo2001@gmail.com>
2023-07-24 16:15:05 +0200

    index.php add

---
+++ index.php
@@ -0,0 +1,26 @@
+<?php
+require_once 'Models/AnimalCategory.php';
+require_once 'Models/Dog.php';
+require_once 'Models/Cat.php';
+require_once 'Models/Bird.php';
+require_once 'Models/Fish.php';
+require_once 'Models/Product.php';
+require_once 'Models/FoodProduct.php';
+require_once 'Models/ToyProduct.php';
+require_once 'Models/AccessoryProduct.php';
+
+?>
+
+
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Animal Shop</title>
+</head>
+
+<body>
+
+</body>
+
+</html>
