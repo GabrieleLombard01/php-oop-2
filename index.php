273c9bdc7cf002c9369fc23da51ee014a596bde0
GabrieleLombard01 <gabriellombardo2001@gmail.com>
2023-07-24 16:49:36 +0200

    product by category

--- index.php
+++ index.php
@@ -14,6 +14,17 @@ $dogCategory = new Dog();
 $catCategory = new Cat();
 $birdCategory = new Bird();
 $fishCategory = new Fish();
+
+// CREO prodotti per ogni categoria
+$products = array(
+    new FoodProduct("Almo Nature Holistic Maintenance Medium Large Tonno e Riso", 12.99, "https://arcaplanet.vtexassets.com/arquivos/ids/245173/almo-nature-holistic-cane-adult-medium-pollo-e-riso.jpg"),
+    new FoodProduct("Almo Nature Cat Daily Lattina", 1.49, "https://arcaplanet.vtexassets.com/arquivos/ids/245336/almo-daily-menu-cat-400-gr-vitello.jpg"),
+    new FoodProduct("Mangime per Pesci Guppy in Fiocchi", 5.99, "https://arcaplanet.vtexassets.com/arquivos/ids/272714/tetra-guppy-mini-flakes.jpg"),
+    new AccessoryProduct("Voliera Wilma in Legno", 89.99, "https://arcaplanet.vtexassets.com/arquivos/ids/258384/voliera-wilma1.jpg"),
+    new AccessoryProduct("Cartucce Filtranti per Filtro EasyCrystal", 8.99, "https://arcaplanet.vtexassets.com/arquivos/ids/272741/tetra-easycrystal-filterpack-250-300.jpg"),
+    new ToyProduct("Kong Classic", 9.99, "https://arcaplanet.vtexassets.com/arquivos/ids/256599/kong-classic1.jpg"),
+    new ToyProduct("Topini di peluche Trixie", 3.99, "https://arcaplanet.vtexassets.com/arquivos/ids/223852/trixie-gatto-gioco-active-mouse-peluche.jpg")
+);
 ?>
 
 
